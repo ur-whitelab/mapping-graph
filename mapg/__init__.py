64310ba0a5a74fe98f4d4ef142c9255bd744f105
Maghesree Chakraborty <mchakra2@ur.rochester.edu>
2017-11-17 15:47:10 -0500

    Added tests for the equivalent class functions

--- mapg/__init__.py
+++ mapg/__init__.py
@@ -1,3 +1,4 @@
 '''mapping graph (mapg)'''
 
 from .reading import smiles2graph, draw
+from .bond_equiv import hash_neighs, bond_equiv_classes
--- tests/test_mapg.py
+++ tests/test_mapg.py
@@ -1,5 +1,6 @@
 import mapg
-
+import networkx as nx
+import operator
 def test_smiles2graph():
     '''Tests a basic conversion from smiles into a graph'''
     graph, line_graph = mapg.smiles2graph('CO')
@@ -21,3 +22,40 @@ def test_draw():
     svg = mapg.draw('CCCO')
     svg2 = mapg.draw('CCCO', [(1, 2), (3, 4)], True)
 
+def test_hash_neighs():
+    '''Tests if the hash_neighs function correctly gives a sub-tree'''
+    graph, line_graph = mapg.smiles2graph('CCCl')
+    for g_node in line_graph.nodes_iter():
+        spath=nx.shortest_path_length(line_graph,source=g_node)
+    max_d=max(spath.items(), key=operator.itemgetter(1))[1]
+    #Identify the CCl node of the line_graph for chloro ethane, which we set as the root
+    for (p, d) in line_graph.nodes(data=True):
+            if d['bond'] == 'CCl':
+                set_root=p
+    test_tree= mapg.hash_neighs(set_root,line_graph,max_depth=max_d)
+    #Checking if the maximum depth of the tree with CCl node as root is 2
+    tree_depth=nx.shortest_path_length(line_graph,source=set_root)
+    max_tree_depth=max(tree_depth.items(), key=operator.itemgetter(1))[1]
+    assert(max_tree_depth==2)
+
+def test_bond_equiv_classes():
+    '''Tests if the function correctly identifies
+    equivalent bonds'''
+    #check the number of equivalent bonds with no local or global symmetry
+    graph1,line_graph1=mapg.smiles2graph('C(F)(Cl)Br')
+    equiv_bond_groups1=mapg.bond_equiv_classes(graph1,line_graph1)
+    #Since there is no symmetry, all the four bonds will have their individual equivalence groups
+    assert(len(equiv_bond_groups1)==4)
+    
+    #check number of bond classes in chloroacetone containing local symmetry
+    graph2, line_graph2=mapg.smiles2graph('CC(=O)CCl')
+    equiv_bond_groups2=mapg.bond_equiv_classes(graph2,line_graph2)
+    assert(len(equiv_bond_groups2)==6)
+
+    #check number of bond classes in neopentane which displays global symmetry
+    graph3, line_graph3=mapg.smiles2graph('CC(C)(C)C')
+    equiv_bond_groups3=mapg.bond_equiv_classes(graph3,line_graph3)
+    assert(len(equiv_bond_groups3)==2)
+            
+    
+    
